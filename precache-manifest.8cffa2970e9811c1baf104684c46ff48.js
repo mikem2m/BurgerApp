378548905453c86da37d3034a9d2ff40a632c10a
mikem2m <michael08ace@gmail.com>
2019-08-03 23:55:16 +0800

    Updates

---
+++ precache-manifest.8cffa2970e9811c1baf104684c46ff48.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b1d2d1cb10fe849c5be9f072cdec3c",
+    "url": "/BurgerApp/index.html"
+  },
+  {
+    "revision": "b9c8abfd886493c67c59",
+    "url": "/BurgerApp/static/css/3.06ab8063.chunk.css"
+  },
+  {
+    "revision": "6837ccc220c23b037008",
+    "url": "/BurgerApp/static/css/4.26eab9ef.chunk.css"
+  },
+  {
+    "revision": "cdb088c3ba032e778a7c",
+    "url": "/BurgerApp/static/css/5.920d2794.chunk.css"
+  },
+  {
+    "revision": "d30ad20537755ce9379a",
+    "url": "/BurgerApp/static/css/main.59063d21.chunk.css"
+  },
+  {
+    "revision": "670abefa7d597ded58cf",
+    "url": "/BurgerApp/static/js/2.273d0267.chunk.js"
+  },
+  {
+    "revision": "b9c8abfd886493c67c59",
+    "url": "/BurgerApp/static/js/3.623fbb7d.chunk.js"
+  },
+  {
+    "revision": "6837ccc220c23b037008",
+    "url": "/BurgerApp/static/js/4.d5b8570e.chunk.js"
+  },
+  {
+    "revision": "cdb088c3ba032e778a7c",
+    "url": "/BurgerApp/static/js/5.6a591e30.chunk.js"
+  },
+  {
+    "revision": "d30ad20537755ce9379a",
+    "url": "/BurgerApp/static/js/main.d5e60eb0.chunk.js"
+  },
+  {
+    "revision": "ebbfe4cb8b69546afa45",
+    "url": "/BurgerApp/static/js/runtime~main.527e61c0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerApp/static/media/burger-logo.b8503d26.png"
+  }
+]);
